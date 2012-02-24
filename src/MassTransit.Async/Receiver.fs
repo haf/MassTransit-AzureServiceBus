b44c40ac7545c541256e12a12a48d39cd82d5a24
Henrik <henrik@haf.se>
2012-02-24 10:48:08 +0100

    code formatting and breaking out into functions

--- src/MassTransit.Async/Receiver.fs
+++ src/MassTransit.Async/Receiver.fs
@@ -27,6 +27,8 @@ open MassTransit.AzureServiceBus
 open MassTransit.Async.Queue
 open MassTransit.Async.AsyncRetry
 
+type internal Agent<'T> = MailboxProcessor<'T>
+
 /// Create a new receiver, with a queue description,
 /// a factory for messaging factories and some control flow data
 type Receiver(desc   : QueueDescription,
@@ -35,21 +37,21 @@ type Receiver(desc   : QueueDescription,
               ?maxReceived : int,
               ?concurrency : int,
               ?mfEveryNthConcurrentAsync : int) =
+  
   /// Field denoting whether the receiver is in the started state or, if false, in the stopped state.
   let mutable started = false
-  let logger = MassTransit.Logging.Logger.Get(typeof<Receiver>)
-  
-  let queueSize = defaultArg maxReceived 50
 
-  /// Field that contains the collection of messages
-  let messages = new BlockingCollection<_>(queueSize)
+  /// The 'scratch' buffer that tunnels messages from the ASB receivers
+  /// to the consumers of the Receiver class.
+  let messages = new BlockingCollection<_>(defaultArg maxReceived 50)
   
+  let logger = MassTransit.Logging.Logger.Get(typeof<Receiver>)
   let timeout = TimeSpan.FromMilliseconds 50.0
   let concurrency = defaultArg concurrency 250
   let nthAsync = defaultArg mfEveryNthConcurrentAsync 100 // in initAsyncs
-
   let azQDesc = desc.Inner
 
+  /// creates an async workflow worker, given a message receiver client
   let worker client =
       async {
         let! cancelled = Async.CancellationToken
@@ -59,7 +61,7 @@ type Receiver(desc   : QueueDescription,
             logger.Debug("received message")
             messages.Add bmsg
           else
-            logger.Debug("got null msg due to timeout receiving") }
+            () }//logger.Debug("got null msg due to timeout receiving") }
 
   /// Starts (stop-1)/100 new clients and message factories, so for stop=501
   /// it loops 500 times and starts 5 new clients
@@ -76,7 +78,14 @@ type Receiver(desc   : QueueDescription,
       | _ ->
         return! initAsyncs desc newMf stop (curr+1) recvs }
 
+  /// All initial message receivers and messaging factories are kicked off and then
+  /// all such receivers and factories are awaited.
   let mfAndRecvsColl = Async.RunSynchronously (initAsyncs desc newMf (concurrency+1) 1 [])
+
+  let start () =
+    desc |> create nm |> Async.RunSynchronously
+    for (mf, client) in mfAndRecvsColl do
+      worker client |> Async.Start
 
   /// Closes the receivers and message factories created
   let closeColl () =
@@ -88,33 +97,48 @@ type Receiver(desc   : QueueDescription,
       if not(recv.IsClosed) then
         try recv.Close()
         with | x -> logger.Error("could not close receiver", x)
+        
+  /// cleans out the message buffer and disposes all messages therein
+  let clearLocks () = 
+    while messages.Count > 0 do
+      async {
+        let m = ref null
+        if messages.TryTake(m, TimeSpan.FromMilliseconds(4.0)) then 
+          try 
+            do! Async.FromBeginEnd((!m).BeginAbandon, (!m).EndAbandon)
+            (!m).Dispose()
+          with 
+          | x ->
+            let entry = sprintf "could not abandon message#%s" <| (!m).MessageId
+            logger.Error(entry, x) }
+        |> Async.Start
+
 
   /// Starts the receiver which starts the consuming from the service bus
   /// and creates the queue if it doesn't exist
   member x.Start () =
-    logger.InfoFormat("started for queue '{0}'", desc)
+    logger.InfoFormat("start called for queue '{0}'", desc)
     if started then ()
-    else
-      started <- true
-      desc |> create nm |> Async.RunSynchronously
-      for (mf, client) in mfAndRecvsColl do
-        worker client |> Async.Start
+    else started <- true
+    start ()
 
-  /// Stops the receiver which closes all messaging factories and message receivers 
-  /// that it holds onto.
+  /// Stops the receiver; allowing it to start once again.
   member x.Stop () =
-    logger.Info("stop called")
+    logger.InfoFormat("stop called for queue '{0}'", desc)
     if not(started) then ()
     else 
       started <- false
       Async.CancelDefaultToken ()
 
+  /// Returns a message if one was added to the buffer within the timeout specified,
+  /// or otherwise returns null.
   member x.Get(timeout : TimeSpan) =
     let mutable item = null
     let _ = messages.TryTake(&item, timeout.Milliseconds)
     item
 
-  member __.TryTake(?timeout) =
+  /// Tries to take an item from the list of messages
+  member internal __.TryTake(?timeout) =
     let timeout = defaultArg timeout <| TimeSpan.FromMilliseconds 50.0
     let mutable item = null
     if messages.TryTake(&item, timeout.Milliseconds)
@@ -127,22 +151,12 @@ type Receiver(desc   : QueueDescription,
         yield messages.Take() }
 
   interface System.IDisposable with
+    /// Cleans out all receivers and factories.
     member x.Dispose () = 
       logger.DebugFormat("dispose called for receiver on '{0}'", desc.Path)
       x.Stop()
       closeColl ()
-      // clear all message locks that we may have
-      while messages.Count > 0 do
-        async {
-          let m = ref null
-          if messages.TryTake(m, TimeSpan.FromMilliseconds(4.0)) then 
-            try do! Async.FromBeginEnd((!m).BeginAbandon, (!m).EndAbandon)
-            with 
-            | x ->
-              let entry = sprintf "could not abandon message#%s" <| (!m).MessageId
-              logger.Error(entry, x) }
-          |> Async.Start
-
+      clearLocks ()
       messages.Dispose()
 
 [<Extension>]
