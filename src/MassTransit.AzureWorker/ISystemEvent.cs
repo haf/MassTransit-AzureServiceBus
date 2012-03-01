e42ba50548fb97219793014036e47f0047f51fab
Henrik <henrik@haf.se>
2012-03-01 16:29:08 +0100

    we don't need this one anymore

--- src/MassTransit.AzureWorker/ISystemEvent.cs
+++
@@ -1,9 +0,0 @@
-namespace MassTransit.AzureWorker
-{
-	/// <summary>
-	/// 	System event representing something that happened within the infrastructure
-	/// </summary>
-	public interface ISystemEvent
-	{
-	}
-}
