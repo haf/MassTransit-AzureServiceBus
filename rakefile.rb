c9147bf4e5806d96f6a37621e018cf9e800ae78f
Henrik <henrik@haf.se>
2012-03-19 16:13:54 +0100

    removed the ruddy nuget targets

--- rakefile.rb
+++ rakefile.rb
@@ -3,8 +3,16 @@
 require 'albacore'
 require 'fileutils'
 
-task :compile do
-  puts "TODO: compile"
+task :ensure_packages do
+  Dir.glob("./src/**/packages.config") do |cfg|
+    sh %Q[src/.nuget/NuGet.exe install "#{cfg}" -o "src/packages"] do |ok, res|
+      puts (res.inspect) unless ok
+    end
+  end
+end
+
+task :compile => :ensure_packages do
+  sh 'msbuild src/MassTransit-AzureServiceBus.sln'
 end
 
 task :test do
