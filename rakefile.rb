26aacbd112c223949749531fd6569551d0116bca
Henrik <henrik@haf.se>
2012-03-19 10:37:32 +0100

    compile and test in stub rakefile

---
+++ rakefile.rb
@@ -0,0 +1,14 @@
+# Copyright Henrik Feldt 2012
+
+require 'albacore'
+require 'fileutils'
+
+task :compile do
+  puts "TODO: compile"
+end
+
+task :test do
+  puts "TODO: test"
+end
+
+task :default => [:compile, :test]
