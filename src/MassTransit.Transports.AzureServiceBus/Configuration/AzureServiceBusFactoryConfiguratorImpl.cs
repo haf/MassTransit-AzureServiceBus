acf073fcbb32edaa85e91ed50a96d2d266d0fc96
David Pfeffer <david@fivepmtechnology.com>
2012-04-17 08:51:54 -0400

    Tabified two files that had spaces.

--- src/MassTransit.Transports.AzureServiceBus/Configuration/AzureServiceBusFactoryConfiguratorImpl.cs
+++ src/MassTransit.Transports.AzureServiceBus/Configuration/AzureServiceBusFactoryConfiguratorImpl.cs
@@ -3,81 +3,81 @@ using MassTransit.AzureServiceBus;
 
 namespace MassTransit.Transports.AzureServiceBus.Configuration
 {
-    public class AzureAzureServiceBusFactoryConfiguratorImpl
-        : AzureServiceBusFactoryConfigurator
-    {
-        private readonly ReceiverSettingsImpl _settings = new ReceiverSettingsImpl();
+	public class AzureAzureServiceBusFactoryConfiguratorImpl
+		: AzureServiceBusFactoryConfigurator
+	{
+		private readonly ReceiverSettingsImpl _settings = new ReceiverSettingsImpl();
 
-        public ITransportFactory Build()
-        {
-            //var tokenProvider = ConfigFactory.CreateTokenProvider();
-            return new TransportFactoryImpl(_settings);
-        }
+		public ITransportFactory Build()
+		{
+			//var tokenProvider = ConfigFactory.CreateTokenProvider();
+			return new TransportFactoryImpl(_settings);
+		}
 
-        public void SetLockDuration(TimeSpan lockDuration)
-        {
-        }
+		public void SetLockDuration(TimeSpan lockDuration)
+		{
+		}
 
-        public void SetDefaultMessageTimeToLive(TimeSpan ttl)
-        {
-        }
+		public void SetDefaultMessageTimeToLive(TimeSpan ttl)
+		{
+		}
 
-        public void SetDeadLetteringOnExpiration(bool enabled)
-        {
-        }
+		public void SetDeadLetteringOnExpiration(bool enabled)
+		{
+		}
 
-        public void SetBatchedOperations(bool enabled)
-        {
-        }
+		public void SetBatchedOperations(bool enabled)
+		{
+		}
 
-        public void SetReceiveTimeout(TimeSpan timeout)
-        {
-            _settings.ReceiveTimeout = timeout;
-        }
+		public void SetReceiveTimeout(TimeSpan timeout)
+		{
+			_settings.ReceiveTimeout = timeout;
+		}
 
-        public void SetReceiverName(string name)
-        {
-            _settings.ReceiverName = name;
-        }
-    }
+		public void SetReceiverName(string name)
+		{
+			_settings.ReceiverName = name;
+		}
+	}
 
-    /// <summary>
-    /// Interface for users to configure the transport with
-    /// </summary>
-    public interface AzureServiceBusFactoryConfigurator
-    {
-        /// <summary>
-        /// Sets the lock duration for the messages consumed from queues
-        /// and topics, which affects the rate of duplicates. The lower the duration,
-        /// the more duplicates you'll get for processes that never finish before
-        /// the lease expires (these messages will pop up again).
-        /// </summary>
-        void SetLockDuration(TimeSpan lockDuration);
+	/// <summary>
+	/// Interface for users to configure the transport with
+	/// </summary>
+	public interface AzureServiceBusFactoryConfigurator
+	{
+		/// <summary>
+		/// Sets the lock duration for the messages consumed from queues
+		/// and topics, which affects the rate of duplicates. The lower the duration,
+		/// the more duplicates you'll get for processes that never finish before
+		/// the lease expires (these messages will pop up again).
+		/// </summary>
+		void SetLockDuration(TimeSpan lockDuration);
 
-        /// <summary>
-        /// Sets the default message time to live, i.e. how
-        /// long it will remain in a queue or topic before
-        /// being removed by the broker.
-        /// </summary>
-        void SetDefaultMessageTimeToLive(TimeSpan ttl);
+		/// <summary>
+		/// Sets the default message time to live, i.e. how
+		/// long it will remain in a queue or topic before
+		/// being removed by the broker.
+		/// </summary>
+		void SetDefaultMessageTimeToLive(TimeSpan ttl);
 
-        /// <summary>
-        /// Sets whether the message should be moved to the built-in
-        /// AZURE (not MassTransit) error poison message queue if the
-        /// message is expired, i.e. if the message is NOT consumed by a 
-        /// consumer.
-        /// </summary>
-        void SetDeadLetteringOnExpiration(bool enabled);
+		/// <summary>
+		/// Sets whether the message should be moved to the built-in
+		/// AZURE (not MassTransit) error poison message queue if the
+		/// message is expired, i.e. if the message is NOT consumed by a 
+		/// consumer.
+		/// </summary>
+		void SetDeadLetteringOnExpiration(bool enabled);
 
-        /// <summary>
-        /// Sets whether batched operations are enabled on
-        /// topics and queues.
-        /// </summary>
-        void SetBatchedOperations(bool enabled);
+		/// <summary>
+		/// Sets whether batched operations are enabled on
+		/// topics and queues.
+		/// </summary>
+		void SetBatchedOperations(bool enabled);
 
-        /// <summary>
-        /// Sets the name for subscriptions.
-        /// </summary>
-        void SetReceiverName(string name);
-    }
+		/// <summary>
+		/// Sets the name for subscriptions.
+		/// </summary>
+		void SetReceiverName(string name);
+	}
 }
--- src/MassTransit.Transports.AzureServiceBus/TransportFactoryImpl.cs
+++ src/MassTransit.Transports.AzureServiceBus/TransportFactoryImpl.cs
@@ -10,145 +10,145 @@ using MassTransit.Transports.AzureServiceBus.Management;
 
 namespace MassTransit.Transports.AzureServiceBus
 {
-    public class TransportFactoryImpl
-        : ITransportFactory
-    {
-        static readonly ILog _logger = Logger.Get(typeof (TransportFactoryImpl));
+	public class TransportFactoryImpl
+		: ITransportFactory
+	{
+		static readonly ILog _logger = Logger.Get(typeof (TransportFactoryImpl));
 
-        private readonly ReaderWriterLockedDictionary<Uri, ConnectionHandler<ConnectionImpl>> _connCache;
-        private readonly ReaderWriterLockedDictionary<Uri, AzureServiceBusEndpointAddress> _addresses;
-        private readonly AzureMessageNameFormatter _formatter;
-        private bool _disposed;
+		private readonly ReaderWriterLockedDictionary<Uri, ConnectionHandler<ConnectionImpl>> _connCache;
+		private readonly ReaderWriterLockedDictionary<Uri, AzureServiceBusEndpointAddress> _addresses;
+		private readonly AzureMessageNameFormatter _formatter;
+		private bool _disposed;
 
-        private readonly ReceiverSettings _settings;
+		private readonly ReceiverSettings _settings;
 
-        public TransportFactoryImpl()
-            : this(new ReceiverSettingsImpl())
-        {
-            
-        }
+		public TransportFactoryImpl()
+			: this(new ReceiverSettingsImpl())
+		{
+			
+		}
 
-        public TransportFactoryImpl(ReceiverSettings settings)
-        {
-            _addresses = new ReaderWriterLockedDictionary<Uri, AzureServiceBusEndpointAddress>();
-            _connCache = new ReaderWriterLockedDictionary<Uri, ConnectionHandler<ConnectionImpl>>();
-            _formatter = new AzureMessageNameFormatter();
+		public TransportFactoryImpl(ReceiverSettings settings)
+		{
+			_addresses = new ReaderWriterLockedDictionary<Uri, AzureServiceBusEndpointAddress>();
+			_connCache = new ReaderWriterLockedDictionary<Uri, ConnectionHandler<ConnectionImpl>>();
+			_formatter = new AzureMessageNameFormatter();
 
-            _settings = settings;
+			_settings = settings;
 
-            _logger.Debug("created new transport factory");
-        }
+			_logger.Debug("created new transport factory");
+		}
 
-        /// <summary>
-        /// 	Gets the scheme. (af-queues)
-        /// </summary>
-        public string Scheme
-        {
-            get { return Constants.Scheme; }
-        }
+		/// <summary>
+		/// 	Gets the scheme. (af-queues)
+		/// </summary>
+		public string Scheme
+		{
+			get { return Constants.Scheme; }
+		}
 
-        public IMessageNameFormatter MessageNameFormatter
-        {
-            get { return _formatter; }
-        }
+		public IMessageNameFormatter MessageNameFormatter
+		{
+			get { return _formatter; }
+		}
 
-        /// <summary>
-        /// 	Builds the duplex transport.
-        /// </summary>
-        /// <param name="settings"> The settings. </param>
-        /// <returns> </returns>
-        public IDuplexTransport BuildLoopback([NotNull] ITransportSettings settings)
-        {
-            if (settings == null) 
-                throw new ArgumentNullException("settings");
+		/// <summary>
+		/// 	Builds the duplex transport.
+		/// </summary>
+		/// <param name="settings"> The settings. </param>
+		/// <returns> </returns>
+		public IDuplexTransport BuildLoopback([NotNull] ITransportSettings settings)
+		{
+			if (settings == null) 
+				throw new ArgumentNullException("settings");
 
-            _logger.Debug("building duplex transport");
+			_logger.Debug("building duplex transport");
 
-            return new Transport(settings.Address, () => BuildInbound(settings), () => BuildOutbound(settings));
-        }
+			return new Transport(settings.Address, () => BuildInbound(settings), () => BuildOutbound(settings));
+		}
 
-        public virtual IInboundTransport BuildInbound(ITransportSettings settings)
-        {
-            EnsureProtocolIsCorrect(settings.Address.Uri);
+		public virtual IInboundTransport BuildInbound(ITransportSettings settings)
+		{
+			EnsureProtocolIsCorrect(settings.Address.Uri);
 
-            var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
-            _logger.DebugFormat("building inbound transport for address '{0}'", address);
+			var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
+			_logger.DebugFormat("building inbound transport for address '{0}'", address);
 
-            var client = GetConnection(address);
+			var client = GetConnection(address);
 
-            return new InboundTransportImpl(address, client, new AzureManagementImpl(settings.PurgeExistingMessages, address), MessageNameFormatter, _settings);
-        }
+			return new InboundTransportImpl(address, client, new AzureManagementImpl(settings.PurgeExistingMessages, address), MessageNameFormatter, _settings);
+		}
 
-        public virtual IOutboundTransport BuildOutbound(ITransportSettings settings)
-        {
-            EnsureProtocolIsCorrect(settings.Address.Uri);
+		public virtual IOutboundTransport BuildOutbound(ITransportSettings settings)
+		{
+			EnsureProtocolIsCorrect(settings.Address.Uri);
 
-            var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
-            _logger.DebugFormat("building outbound transport for address '{0}'", address);
+			var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
+			_logger.DebugFormat("building outbound transport for address '{0}'", address);
 
-            var client = GetConnection(address);
+			var client = GetConnection(address);
 
-            return new OutboundTransportImpl(address, client);
-        }
+			return new OutboundTransportImpl(address, client);
+		}
 
-        public virtual IOutboundTransport BuildError(ITransportSettings settings)
-        {
-            EnsureProtocolIsCorrect(settings.Address.Uri);
+		public virtual IOutboundTransport BuildError(ITransportSettings settings)
+		{
+			EnsureProtocolIsCorrect(settings.Address.Uri);
 
-            var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
-            _logger.DebugFormat("building error transport for address '{0}'", address);
+			var address = _addresses.Retrieve(settings.Address.Uri, () => AzureServiceBusEndpointAddressImpl.Parse(settings.Address.Uri));
+			_logger.DebugFormat("building error transport for address '{0}'", address);
 
-            var client = GetConnection(address);
-            return new OutboundTransportImpl(address, client);
-        }
+			var client = GetConnection(address);
+			return new OutboundTransportImpl(address, client);
+		}
 
-        /// <summary>
-        /// 	Ensures the protocol is correct.
-        /// </summary>
-        /// <param name="address"> The address. </param>
-        private void EnsureProtocolIsCorrect(Uri address)
-        {
-            if (address.Scheme != Scheme)
-                throw new EndpointException(address,
-                                            string.Format("Address must start with 'stomp' not '{0}'", address.Scheme));
-        }
+		/// <summary>
+		/// 	Ensures the protocol is correct.
+		/// </summary>
+		/// <param name="address"> The address. </param>
+		private void EnsureProtocolIsCorrect(Uri address)
+		{
+			if (address.Scheme != Scheme)
+				throw new EndpointException(address,
+											string.Format("Address must start with 'stomp' not '{0}'", address.Scheme));
+		}
 
-        private ConnectionHandler<ConnectionImpl> GetConnection(AzureServiceBusEndpointAddress address)
-        {
-            _logger.DebugFormat("get connection( address:'{0}' )", address);
+		private ConnectionHandler<ConnectionImpl> GetConnection(AzureServiceBusEndpointAddress address)
+		{
+			_logger.DebugFormat("get connection( address:'{0}' )", address);
 
-            return _connCache.Retrieve(address.Uri, () =>
-                {
-                    var connection = new ConnectionImpl(address);
-                    var connectionHandler = new ConnectionHandlerImpl<ConnectionImpl>(connection);
+			return _connCache.Retrieve(address.Uri, () =>
+				{
+					var connection = new ConnectionImpl(address);
+					var connectionHandler = new ConnectionHandlerImpl<ConnectionImpl>(connection);
 
-                    return connectionHandler;
-                });
-        }
+					return connectionHandler;
+				});
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        protected virtual void Dispose(bool managed)
-        {
-            if (_disposed)
-                return;
+		protected virtual void Dispose(bool managed)
+		{
+			if (_disposed)
+				return;
 
-            if (managed)
-            {
-                _connCache.Values.Each(x => x.Dispose());
-                _connCache.Clear();
-                _connCache.Dispose();
+			if (managed)
+			{
+				_connCache.Values.Each(x => x.Dispose());
+				_connCache.Clear();
+				_connCache.Dispose();
 
-                _addresses.Values.Each(x => x.Dispose());
-                _addresses.Clear();
-                _addresses.Dispose();
-            }
+				_addresses.Values.Each(x => x.Dispose());
+				_addresses.Clear();
+				_addresses.Dispose();
+			}
 
-            _disposed = true;
-        }
-    }
+			_disposed = true;
+		}
+	}
 }
